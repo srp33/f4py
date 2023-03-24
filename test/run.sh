e36cccc6eaf2ca1038c6ce1080da5818156d5f0b
Stephen Piccolo <stephen.piccolo.byu@gmail.com>
2023-03-24 13:19:06 +1300

    Updated TODO list

--- test/run.sh
+++ test/run.sh
@@ -18,19 +18,11 @@ rm -rf f4
 cp -r ../src/f4 .
 
 dockerCommand="docker run -i -t --rm --platform linux/x86_64 --user $(id -u):$(id -g) -v $(pwd):/sandbox -v $(pwd)/data:/data -v /tmp:/tmp --workdir=/sandbox srp33/f4_test"
-##dockerCommand="docker run -d --rm --platform linux/x86_64 --user $(id -u):$(id -g) -v $(pwd):/sandbox -v $(pwd)/data:/data -v /tmp:/tmp --workdir=/sandbox srp33/f4_test"
 
 #$dockerCommand bash -c "time python3 build_tsv.py 10 10 10 10000 data/medium.tsv"
 
 #$dockerCommand bash -c "time python3 build_tsv.py 250 250 500 1000000 data/large_tall.tsv"
 #$dockerCommand bash -c "time python3 build_tsv.py 250000 250000 500000 1000 data/large_wide.tsv"
-
-#TODO: Integrate f4 into the analysis paper tests. Check speed and optimize more, if needed.
-#        Try potential other speed improvements:
-#          * Python 3.11
-#          * PyPi.
-#          * https://github.com/exaloop/codon
-#          * https://nuitka.net (compiles your Python code to C, is supposed to achieve speedups of 3x or greater).
 
 #######################################################
 # Run tests
