00ac109acbad29b2976046549a96e78c3895e252
Stephen Piccolo <stephen.piccolo.byu@gmail.com>
2024-04-26 07:15:18 -0600

    Changed tests

--- test/test.py
+++ test/test.py
@@ -870,26 +870,26 @@ def run_super_test(description, fltr, select_columns, num_parallel, tmp_dir_path
 
     print(f"  {elapsed_time:.2f} seconds, {num_lines} lines in output file")
 
-run_all_small_tests()
-sys.exit()
+#run_all_small_tests()
+#sys.exit()
 
 for compression_type in [None]:
 #for compression_type in ["zstd"]:
 #for compression_type in [None, "zstd"]:
 #    num_parallel = 1
-    num_parallel = 4
-#    num_parallel = 16
+#    num_parallel = 4
+    num_parallel = 16
     build_outputs = True
     #build_outputs = False
     verbose = True
     #verbose = False
     check_outputs = True
     #check_outputs = False
-    use_memory_mapping=True
-    #use_memory_mapping=False
+    #use_memory_mapping=True
+    use_memory_mapping=False
 
     # Medium tests
-    run_larger_tests(num_parallel=num_parallel, size="medium", extension="", discrete1_index=11, numeric1_index=21, build_outputs=build_outputs, compression_type=compression_type, check_outputs=check_outputs, verbose=verbose, tmp_dir_path="/tmp/medium", use_memory_mapping=use_memory_mapping)
+#    run_larger_tests(num_parallel=num_parallel, size="medium", extension="", discrete1_index=11, numeric1_index=21, build_outputs=build_outputs, compression_type=compression_type, check_outputs=check_outputs, verbose=verbose, tmp_dir_path="/tmp/medium", use_memory_mapping=use_memory_mapping)
 
     # Large tests
 #    run_larger_tests(num_parallel=num_parallel, size="large_tall", extension="", discrete1_index=251, numeric1_index=501, build_outputs=build_outputs, compression_type=compression_type, check_outputs=check_outputs, verbose=verbose, tmp_dir_path="/tmp/large_tall", use_memory_mapping=use_memory_mapping)
@@ -912,8 +912,8 @@ for compression_type in [None]:
 
     #Attempt this? https://community.hpe.com/t5/servers-systems-the-right/cray-graph-engine-takes-on-a-trillion-triples/ba-p/7096770
 
-    f4.transpose("data/medium.f4", "/tmp/medium_transposed.f4", src_column_for_names="ID", num_parallel=num_parallel, verbose=verbose, use_memory_mapping=use_memory_mapping)
-#    f4.transpose("data/large_tall.f4", "/tmp/large_tall_transposed.f4", src_column_for_names="ID", num_parallel=num_parallel, verbose=verbose, use_memory_mapping=use_memory_mapping)
+#    f4.transpose("data/medium.f4", "/tmp/medium_transposed.f4", src_column_for_names="ID", num_parallel=num_parallel, verbose=verbose, use_memory_mapping=use_memory_mapping)
+    f4.transpose("data/large_tall.f4", "/tmp/large_tall_transposed.f4", src_column_for_names="ID", num_parallel=num_parallel, verbose=verbose, use_memory_mapping=use_memory_mapping)
 
 #    f4.inner_join("data/medium.f4", "data/medium.f4", "ID", "/tmp/medium_joined.f4", num_parallel=num_parallel, verbose=verbose, use_memory_mapping=use_memory_mapping)
 #    f4.inner_join("data/large_tall.f4", "data/large_wide.f4", "ID", "/tmp/large_joined.f4", num_parallel=num_parallel, verbose=verbose, use_memory_mapping=use_memory_mapping)
