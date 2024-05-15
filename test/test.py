86dba04808dba45a6bf2b064d1d5d9c21203008f
Stephen Piccolo <stephen.piccolo.byu@gmail.com>
2024-05-15 16:42:07 -0400

    Reduced logging

--- test/test.py
+++ test/test.py
@@ -875,20 +875,20 @@ def run_super_test(description, fltr, select_columns, num_parallel, tmp_dir_path
 #run_all_small_tests()
 #sys.exit()
 
-#for compression_type in [None]:
+for compression_type in [None]:
 #for compression_type in ["zstd"]:
-for compression_type in [None, "zstd"]:
+#for compression_type in [None, "zstd"]:
 #    num_parallel = 1
-    num_parallel = 4
-#    num_parallel = 16
+#    num_parallel = 4
+    num_parallel = 16
     build_outputs = True
     #build_outputs = False
     verbose = True
     #verbose = False
     check_outputs = True
     #check_outputs = False
-    #use_memory_mapping=True
-    use_memory_mapping=False
+    use_memory_mapping=True
+    #use_memory_mapping=False
 
     # Medium tests
     run_larger_tests(num_parallel=num_parallel, size="medium", extension="", discrete1_index=11, numeric1_index=21, build_outputs=build_outputs, compression_type=compression_type, check_outputs=check_outputs, verbose=verbose, tmp_dir_path="/tmp/medium", use_memory_mapping=use_memory_mapping)
