08b982ac63659babaaa3db59b20b754ac7469c91
Stephen Piccolo <stephen.piccolo.byu@gmail.com>
2024-05-15 20:34:43 -0600

    Tweaked tests

--- test/test.py
+++ test/test.py
@@ -884,8 +884,8 @@ for compression_type in [None]:
     num_parallel = 16
     build_outputs = True
     #build_outputs = False
-    verbose = True
-    #verbose = False
+    #verbose = True
+    verbose = False
     check_outputs = True
     #check_outputs = False
     use_memory_mapping=True
@@ -894,10 +894,10 @@ for compression_type in [None]:
     do_test_with_indexing = False
 
     # Medium tests
-    run_larger_tests(num_parallel=num_parallel, size="medium", extension="", discrete1_index=11, numeric1_index=21, build_outputs=build_outputs, compression_type=compression_type, check_outputs=check_outputs, verbose=verbose, tmp_dir_path="/tmp/medium", use_memory_mapping=use_memory_mapping, do_test_with_indexing=do_test_with_indexing)
+#    run_larger_tests(num_parallel=num_parallel, size="medium", extension="", discrete1_index=11, numeric1_index=21, build_outputs=build_outputs, compression_type=compression_type, check_outputs=check_outputs, verbose=verbose, tmp_dir_path="/tmp/medium", use_memory_mapping=use_memory_mapping, do_test_with_indexing=do_test_with_indexing)
 
     # Large tests
-#    run_larger_tests(num_parallel=num_parallel, size="large_tall", extension="", discrete1_index=251, numeric1_index=501, build_outputs=build_outputs, compression_type=compression_type, check_outputs=check_outputs, verbose=verbose, tmp_dir_path="/tmp/large_tall", use_memory_mapping=use_memory_mapping, do_test_with_indexing=do_test_with_indexing)
+    run_larger_tests(num_parallel=num_parallel, size="large_tall", extension="", discrete1_index=251, numeric1_index=501, build_outputs=build_outputs, compression_type=compression_type, check_outputs=check_outputs, verbose=verbose, tmp_dir_path="/tmp/large_tall", use_memory_mapping=use_memory_mapping, do_test_with_indexing=do_test_with_indexing)
 #    run_larger_tests(num_parallel=num_parallel, size="large_wide", extension="", discrete1_index=250001, numeric1_index=500001, build_outputs=build_outputs, compression_type=compression_type, check_outputs=check_outputs, verbose=verbose, tmp_dir_path="/tmp/large_wide", use_memory_mapping=use_memory_mapping, do_test_with_indexing=do_test_with_indexing)
 #
 #    run_super_tests(num_parallel=num_parallel, size="test_tall", extension=".gz", compression_type=compression_type, verbose=verbose, tmp_dir_path="/tmp/test_tall", use_memory_mapping=use_memory_mapping)
